d456e3bfaba320a9b27880526d338298bbc5ed05
bogdanice <bogdan_ionita@outlook.com>
2022-12-12 10:33:22 +0000

    Add methods to read csv files and store the resulting dataframes as table

--- papp/pmodule/SparkApp.py
+++ papp/pmodule/SparkApp.py
@@ -1,5 +1,5 @@
 from pyspark import SparkConf
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 
 import pmodule.settings as settings
 
@@ -22,5 +22,14 @@ class SparkApp:
 
 
     def countFromTable(self, table) -> int:
-        cnt = self._spark.sql("SELECT COUNT(*) AS cnt FROM {}".format(table))
-        return cnt
+        return self._spark.sql("SELECT COUNT(*) AS cnt FROM {}".format(table))
+
+    
+    def readCsvFileToDataFrame(self, filePath, schema, delimiter=",", header=True) -> DataFrame:
+        return self._spark.read.format("csv") \
+                        .option(delimiter=delimiter, header=True) \
+                        .schema(schema) \
+                        .load(filePath)
+
+    def storeDataFrameAsTable(self, df, tableName):
+        df.write.saveAsTable(name=table_name, format="parquet", mode="overwrite", compress=False)
